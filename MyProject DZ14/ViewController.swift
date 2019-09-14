0b041bd52d9210cc6631eb77c6a63a86be3b2747
Sergey <gorsheninsg@gmail.com>
2019-09-14 11:02:46 +0300

    Initial Commit

---
+++ MyProject DZ14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProject DZ14
+//
+//  Created by Sergey Gorshenin on 14/09/2019.
+//  Copyright © 2019 Sergey Gorshenin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
